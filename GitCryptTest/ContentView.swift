f6cc761e3ca1ed99f7f9907f3b0eef80d55d1854
Peter van den Hamer <github@vdhamer.com>
2022-07-17 16:25:22 +0200

    Initial Commit

---
+++ GitCryptTest/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GitCryptTest
+//
+//  Created by Peter van den Hamer on 17/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitCryptTest/GitCryptTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCryptTestApp.swift
+//  GitCryptTest
+//
+//  Created by Peter van den Hamer on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitCryptTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
